052203e93e2a80caa507ff68435f0e719b8167f1
Alpine Devcamp <alpine@devcamp.com>
2017-06-26 15:09:16 +0000

    Configured custom routes for pages

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,18 @@ class PortfoliosController < ApplicationController
      @portfolio_item = Portfolio.find(params[:id])
    end
 
+  def destroy
+   
+    @portfolio_item = Portfolio.find(params[:id])
+   
+   
+    @portfolio_item.destroy
+   
+    
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'record was removed.' }
+      format.json { head :no_content }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
   resources :blogs
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+ 
+  root to:'pages#home'
+  end
