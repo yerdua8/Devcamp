91ea1c4af820092cec32bd666510da7aeb1280b5
Alpine Devcamp <alpine@devcamp.com>
2017-06-27 17:49:51 +0000

    Implemented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolio#show'
 
   get 'about-me', to: 'pages#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+3.times do |topic|
+  Topic.create!(
+      title: "Topic#{topic}"
+  )
+end
+
+puts "3 Topics created"
 
 10.times do |blog|
     Blog.create!(
@@ -16,7 +23,8 @@
       quis nostrum exercitationem ullam corporis 
       ex ea commodi consequatur? Quis autem vel eum iure 
       qui in ea voluptate velit esse quam nihil molestiae 
-      illum qui dolorem eum fugiat quo voluptas nulla"
+      illum qui dolorem eum fugiat quo voluptas nulla",
+      topid_id: Topic.last.id
         )
 end
 
@@ -28,10 +36,10 @@ end
 end
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
-        subtitle: "My great service" ,
+        subtitle: "Ruby on Rails",
         body: "Lorem ipsum dolor sit amet, consectetur 
         adipiscing elit, sed do eiusmod tempor incididunt 
         ut labore et dolore magna aliqua. Ut enim ad 
@@ -47,3 +55,26 @@ puts "5 skills created"
         
     )
 end
+
+
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Angular",
+        body: "Lorem ipsum dolor sit amet, consectetur 
+        adipiscing elit, sed do eiusmod tempor incididunt 
+        ut labore et dolore magna aliqua. Ut enim ad 
+        minim veniam, quis nostrud exercitation ullamco 
+        laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id 
+        est laborum." ,
+        main_image: "http://via.placeholder.com/600x400" ,
+        thumb_image: "http://via.placeholder.com/350x200"
+        
+    )
+end
+
+puts "9 portfolio items created"
