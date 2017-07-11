ea92bedf677d77b1d5fa49906462c7d834145c5b
Alpine Devcamp <alpine@devcamp.com>
2017-07-11 16:18:58 +0000

    added nav bar added default header content

--- config/application.rb
+++ config/application.rb
@@ -16,16 +16,9 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MyApp
+module DevcampPortfolio
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.action_controller.
+      permit_all_parameters = true
   end
 end
