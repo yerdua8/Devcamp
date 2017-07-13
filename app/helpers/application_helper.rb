a70408d3300c4230e65239ed977f0826899b6f4f
Alpine Devcamp <alpine@devcamp.com>
2017-07-13 14:29:12 +0000

    Implemented content tag helper items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,12 @@ module ApplicationHelper
        "<br>".html_safe +
       (link_to "Login", new_user_session_path) 
     end 
+  end 
+  def source_helper(layout_name)
+     if session[:source] 
+       greeting = "Thanks for visiting me from  #{session[:source]} and you are on the#{layout_name} layout"
+       content_tag(:p, greeting, class: "source-greeting")
+     end 
+      
   end
 end
