d183c9297a57190a07f00cd453e1aad5db49fd36
Alpine Devcamp <alpine@devcamp.com>
2017-07-18 17:42:16 +0000

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -5,12 +5,13 @@ extend ActiveSupport::Concern
     super || guest_user
   end
   
-def guest_user
-    OpenStruct.new(name: "Guest User", 
-                   first_name: "Guest", 
-                   last_name: "User", 
-                   email: "guest@example.com"
-                   )
-end 
+  def guest_user
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "User"
+    guest.email = "guest@example.com"
+    guest
+   
+  end 
 end
 
