ea644f4006b1765df6bc172aa1cba2d9f5735cd5
Alpine Devcamp <alpine@devcamp.com>
2017-07-27 16:13:29 +0000

    Javascript 1

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,10 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
-
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def angular
@@ -33,17 +32,13 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
-
       else
         format.html { render :edit }
       end
     end
-
   end
 
   def show
@@ -56,21 +51,20 @@ class PortfoliosController < ApplicationController
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
-
     end
   end
 
   private
-  
+
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
-                                      :subtitle, 
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
                                       :body,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
+
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
-    
 end
