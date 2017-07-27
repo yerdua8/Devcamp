929325e666fe06a673a7a10dfd646bd1902979ef
Alpine Devcamp <alpine@devcamp.com>
2017-07-27 16:20:46 +0000

    Push 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :portfolios, except: [:show]
-
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
   get 'angular-items', to: 'portfolios#angular'
-
-  get 'portfolios/:id', to: 'portfolios#show'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-
- 
-  root to:'pages#home'
-  end
+  
+  root to: 'pages#home'
+end
