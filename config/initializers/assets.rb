f729e0711264d6e7c7900b3c2454b16415fd503b
Alpine Devcamp <alpine@devcamp.com>
2017-08-02 17:43:48 +0000

    final changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( blogs.css )
-Rails.application.config.assets.precompile += %w( portfolios.css )
+Rails.application.config.assets.precompile += %w( portfolios.scss )
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts") 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,16 @@ user.create!(
   roles:"site_admin"
   )
   
-  
+  puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created
   
 
 3.times do |topic|
